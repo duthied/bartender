afb1016d3be6422e31aad1496970c0ca3f2e0284
Devlon Duthie <duthied@gmail.com>
2019-09-23 14:20:11 -0400

    Prettier

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // based on the tutorial found here:
 // https://developer.okta.com/blog/2019/05/29/build-crud-nodejs-graphql
 
-import schema from './types/schema.js';
+import schema from "./types/schema.js";
 
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer } = require("apollo-server");
 
-const resolvers = require('./resolvers');
+const resolvers = require("./resolvers");
 
 const server = new ApolloServer({ typeDefs: schema, resolvers: resolvers });
 
 server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);    // eslint-disable-line no-console
+  console.log(`🚀  Server ready at ${url}`); // eslint-disable-line no-console
 });
--- utils.js
+++ utils.js
@@ -1,4 +1,4 @@
-import uuid from 'uuid/v4';
+import uuid from "uuid/v4";
 
 const spirits = {};
 const recipes = {};
@@ -6,29 +6,41 @@ const recipes = {};
 // TODO: add unique name check
 const addSpirit = spirit => {
   const id = uuid();
-  return spirits[id] = { ...spirit, id };
-}
+  return (spirits[id] = { ...spirit, id });
+};
 
 const addRecipe = recipe => {
   const id = uuid();
-  return recipes[id] = { ...recipe, id };
-}
+  return (recipes[id] = { ...recipe, id });
+};
 
 // initial spirits
-let hendricks = addSpirit({ name: "Hendrik's Gin", type: "GIN", howMuchLeft: "50" });
-let havanaClub = addSpirit({ name: "Havana Club", type: "RUM",howMuchLeft: "25" });
+let hendricks = addSpirit({
+  name: "Hendrik's Gin",
+  type: "GIN",
+  howMuchLeft: "50"
+});
+let havanaClub = addSpirit({
+  name: "Havana Club",
+  type: "RUM",
+  howMuchLeft: "25"
+});
 // eslint-disable-next-line no-unused-vars
 let woodford = addSpirit({ name: "Woodford Reserve", type: "BOURBON" });
 
-addRecipe({ 
-  name: "Mucky-Muck", 
+addRecipe({
+  name: "Mucky-Muck",
   ingredients: [
-    { 
-      spirit: hendricks, amount: "2oz" 
-    }, { 
-      spirit: havanaClub, amount: "2oz" 
-    }], 
-    glass: "COLLINS"
+    {
+      spirit: hendricks,
+      amount: "2oz"
+    },
+    {
+      spirit: havanaClub,
+      amount: "2oz"
+    }
+  ],
+  glass: "COLLINS"
 });
 
 module.exports = {
