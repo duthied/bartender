bb7b289a8e2a90717609900d6bd8ed426a771be5
Devlon Duthie <duthied@gmail.com>
2019-10-11 12:04:08 -0400

    Adds request logging

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 // based on the tutorial found here:
 // https://developer.okta.com/blog/2019/05/29/build-crud-nodejs-graphql
 
@@ -7,7 +8,31 @@ const { ApolloServer } = require("apollo-server");
 
 const resolvers = require("./resolvers");
 
-const server = new ApolloServer({ typeDefs: schema, resolvers: resolvers });
+const { print } = require("graphql");
+
+class BasicLogging {
+  requestDidStart({ queryString, parsedQuery, variables }) {
+    const query = queryString || print(parsedQuery);
+    console.log(query);
+    console.log(variables);
+  }
+
+  willSendResponse({ graphqlResponse }) {
+    console.log(JSON.stringify(graphqlResponse, null, 2));
+  }
+}
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   extensions: [() => new BasicLogging()]
+// });
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers: resolvers,
+  extensions: [() => new BasicLogging()]
+});
 
 server.listen().then(({ url }) => {
   console.log(`🚀  Server ready at ${url}`); // eslint-disable-line no-console
